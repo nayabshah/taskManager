99477d026aa1e24d60fea35306dd63a2e33515b2
Ariba <aribaonsocial@gmail.com>
2024-03-11 14:44:20 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,9 @@ app.use("/api", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
-    )
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
